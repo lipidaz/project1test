7e3bfa81a9b4797cde9b9a7dbe6244bcbd65df99
lipidaz <greendragon0812@gmail.com>
2022-08-07 13:00:05 +0700

    test

---
+++ data.js
@@ -0,0 +1,22 @@
+let shopData = [
+    {
+      id: 1,
+      name: 'Cheese Bread',
+      des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.Tenetur itaque corrupti molestias reprehenderit',
+      img : 'https://designs.vn/wp-content/images/09-08-2013/logo_lagi_8_resize.JPG',
+      price: 25000
+    },
+    {
+      id: 2,
+      name: 'Ham Bread',
+      des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.Tenetur itaque corrupti molestias reprehenderit',
+      img : 'https://designs.vn/wp-content/images/09-08-2013/logo_lagi_8_resize.JPG',
+      price: 20000
+    },
+    {
+      id: 3,
+      name: 'Vegan Bread',
+      des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.Tenetur itaque corrupti molestias reprehenderit',
+      img : 'https://designs.vn/wp-content/images/09-08-2013/logo_lagi_8_resize.JPG',
+      price: 30000
+    }]
